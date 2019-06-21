9bb40b6ce5d1cbdd577b32a5cdfbfd1c2e37e2b8
Santhanamariammal <sandalyamuna@gmail.com>
2019-06-21 18:50:05 +0530

    changes in  accredit version folder

--- manifests/setup/install.pp
+++ manifests/setup/install.pp
@@ -6,7 +6,7 @@
 #   include accredit::setup::install
 class accredit::setup::install {
 
-  file { ['/opt/notus', '/opt/notus/node'] :
+  file { ['/opt/accredit', '/opt/accredit/node',"/opt/accredit/node/$accredit::version"] :
     ensure => 'directory'
   }
 
@@ -30,37 +30,37 @@ class accredit::setup::install {
       provider => 'npm',
     }
 
-  exec { 'download_notus_package' :
-    command => '/usr/local/bin/aws s3 cp s3://centizenapps-jenkins/builds/accredit-express/express.tar .',
-    cwd     => '/opt/notus',
+  exec { 'download_accredit_package' :
+    command => "/usr/local/bin/aws s3 cp s3://centizen-jenkins/builds/accredit/$accredit::version/express.tar $accredit::version-express.tar",
+    cwd     => '/opt/accredit',
     path    => '/usr/local/bin/:/bin/:/sbin/:/usr/bin/:/usr/sbin/',
     notify  => Exec['extract_currentbuild'],
-    unless  => 'test -f /opt/notus/express.tar',
+    unless  => "test -f /opt/accredit/$accredit::version-express.tar",
   }
 
   exec { 'extract_currentbuild' :
-    command     => 'tar -xvf /opt/notus/express.tar -C /opt/notus/node',
+    command     => "tar -xvf /opt/accredit/$accredit::version-express.tar -C /opt/accredit/node/$accredit::version",
     path        => '/bin/:/sbin/:/usr/bin/:/usr/sbin/',
     require     => Exec['download_notus_package'],
     refreshonly => true,
   }
 
-  exec { 'download_notusdist_package' :
-    command => '/usr/local/bin/aws s3 cp s3://centizenapps-jenkins/builds/accredit-dist/dist.tar .',
+  exec { 'download_accreditdist_package' :
+    command => "/usr/local/bin/aws s3 cp s3://centizen-jenkins/builds/accredit/$accredit::version/dist.tar $accredit::version-dist.tar",
     cwd     => '/var/www',
     path    => '/usr/local/bin/:/bin/:/sbin/:/usr/bin/:/usr/sbin/',
     notify  => Exec['extract_build'],
-    unless  => 'test -f /var/www/dist.tar',
+    unless  => "test -f /var/www/$accredit::version-dist.tar",
   }
 
   exec { 'extract_build' :
-    command     => 'tar -xvf /var/www/dist.tar --strip-components 1 -C /var/www/html',
+    command     => "tar -xvf /var/www/$accredit::version-dist.tar --strip-components 1 -C /var/www/html/$accredit::version",
     path        => '/bin/:/sbin/:/usr/bin/:/usr/sbin/',
     require     => Exec['download_notusdist_package'],
     refreshonly => true,
   }
 
-  exec { 'chown -R www-data:www-data /var/www/html' :
+  exec { "chown -R www-data:www-data /var/www/html/$accredit::version" :
       path    => '/bin/:/sbin/:/usr/bin/:/usr/sbin/',
       require => Exec['extract_build'],
     }
